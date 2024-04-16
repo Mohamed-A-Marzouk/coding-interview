487b0b2b80250c3b92d69f09662f8df13070ea45
MOHAMED MARZOUK <mohamed.marzouk2@dxc.com>
2024-04-16 12:55:25 +0200

    java parameterized test

--- coding-interview/src/test/java/TestGame.java
+++ coding-interview/src/test/java/TestGame.java
@@ -4,13 +4,37 @@ import org.example.Result;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
-
+@RunWith(Parameterized.class)
 public class TestGame {
 
     Game game = new Game();
+    private Move move1;
+    private Move move2;
+    private Result result;
 
+    public TestGame(Result result, Move move1, Move move2) {
+        this.move1 = move1;
+        this.move2 = move2;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> testCondition(){
+        Object[][] outPut ={
+            {Result.DRAW,Move.ROCK,Move.ROCK},
+                {Result.WIN,Move.PAPER,Move.ROCK},
+                {Result.LOSE,Move.SCISSORS,Move.ROCK},
+                {Result.LOSE,Move.SCISSORS,Move.SCISSORS}
+
+        };
+        return Arrays.asList(outPut);
+    }
     @Test
     public void testGame(){
         game.playGame(100);
@@ -27,5 +51,10 @@ public class TestGame {
     @Test
     public void testGetRoundResultWithScissorsMove(){
         assertEquals(Result.LOSE,game.getRoundResult(Move.SCISSORS,Move.ROCK));
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(result, game.getRoundResult(move1,move2));
     }
 }
